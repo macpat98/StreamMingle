10826e623b9b3ab7a34e9b6be516f0c013db9036
pat.maciejewski98@gmail.com <pat.maciejewski98@gmail.com>
2023-06-04 22:48:38 +0200

    Add home page

---
+++ StreamMingle/src/app/app.constants.ts
@@ -0,0 +1,9 @@
+export class AppConstants {
+    public static readonly Description: string = `
+    Welcome to StreamMingle, your one-stop destination for movies and series!
+    StreamMingle brings together a curated selection of the best content from popular platforms like
+    Netflix, Amazon Prime, HBO Max, and more.
+    Explore a vast collection of movies and TV shows across genres, all in one convenient place.
+    Find your favorites, discover new gems, and enjoy seamless streaming.
+    Elevate your entertainment experience with StreamMingle today!`;
+}
